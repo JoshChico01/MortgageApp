24f7226759b60382e80bbe06960cfc9da939b593
JoshChico01 <joshuacicolari@gmail.com>
2024-05-10 16:34:09 +0800

    Fix text error

--- mortgage_app.py
+++ mortgage_app.py
@@ -54,9 +54,9 @@ def run(interest, loan_amount, repayment_fortnightly):
                  Mortgage amount at the end of each year""")
         st.bar_chart(df, x = "Years")
         st.write(f"""
-                 Loan Amount: {format(start_loan,"###,###")}\n
-                 Total Repayments: {format(round(total_repayments),"###,###")}\n
-                 Interest Paid: {format(round(total_repayments - loan_amount), "###,###")}
+                 Loan Amount: {start_loan:_}\n
+                 Total Repayments: {round(total_repayments):_}\n
+                 Interest Paid: {round(total_repayments - loan_amount):_}
 """)
 
 
